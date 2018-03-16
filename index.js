4f51116098b76408b673b0066c44d878778a57b3
Ju111 <SchneiderJu@gmx.net>
2018-03-16 14:16:52 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-var Meetup = require("meetup")
-var mup = new Meetup()
-
+const Meetup = require("meetup")
+const mup = new Meetup()
 let topicsCounter = {}
+
+const INTERESTS = [
+  'Software Development'
+]
+
+// count the occurrences of each topic
+function countOccurrences(topicNames) {
+  topicNames.forEach(name => {
+    if (!topicsCounter[name]) return topicsCounter[name] = 1
+    topicsCounter[name]++
+  })
+}
+
+// sort the array of topics to create a top10
+function topTen() {
+  return Object.keys(topicsCounter)
+    .sort((topicA, topicB) => (topicsCounter[topicB] - topicsCounter[topicA]))
+    .slice(0, 10)
+    .map(topic => ({
+      topic,
+      count: topicsCounter[topic]
+    }))
+}
+
+function isInterestingTopic(topics) {
+  return topics
+    .filter(topic => (INTERESTS.includes(topic)))
+    .length > 0
+}
 
 mup.stream("/2/rsvps", stream => {
   stream
     .on("data", item => {
-      console.log('-------------------')
-
       const topicNames = item.group.group_topics.map(topic => topic.topic_name)
 
-      if (topicNames.includes('Software Development')) {
+      if (!isInterestingTopic(topicNames)) return
 
-      const arrayOfTopics = Object.keys(topicsCounter)
+      countOccurrences(topicNames)
 
-      topicNames.forEach(name => {
-        if (topicsCounter[name]) {
-          topicsCounter[name]++
-        }
-        else {
-          topicsCounter[name] = 1
-        }
-      })
-
-      arrayOfTopics.sort((topicA, topicB) => {
-        if (topicsCounter[topicA] > topicsCounter[topicB]) {
-          return -1
-        }
-        else if (topicsCounter[topicB] > topicsCounter[topicA]) {
-          return 1
-        }
-        else {
-          return 0
-        }
-      })
-
-      const top10 = arrayOfTopics.slice(0, 10)
-
-      const nameCounter = top10.map(topic => ({
-        topic,
-        count: topicsCounter[topic]
-      }))
-
-      console.log(nameCounter)
-    }
+      console.log(topTen())
     }).on("error", e => {
        console.log("error! " + e)
     });
